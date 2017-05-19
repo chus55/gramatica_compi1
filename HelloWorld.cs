a344516170e1fecef14255de16c55f5d4485b552
AstroThing <andres.rodriguezhn@gmail.com>
2017-05-18 19:33:22 -0600

    Some considerations taken care of

---
+++ HelloWorld.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Object;
+
+public class Hello2
+{
+   public static void Main()
+   {
+      Console.WriteLine("Hello, World!");
+   }
+
+}
+
+public class Hello2
+{
+   public static void Main()
+   {
+      Console.WriteLine("Hello, World!");
+   }
+
+}
+
+public class Hello2
+{
+   public static void Main()
+   {
+      Console.WriteLine("Hello, World!");
+   }
+
+}
